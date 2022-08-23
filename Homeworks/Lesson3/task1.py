14e7c4ae1b440d266e3919891a3f29f60cacdb6f
Андрей Войнено <andriivoinenko@MacBook-Air-Andrej.local>
2022-08-23 19:04:20 +0300

    commit

---
+++ Homeworks/Lesson3/task1.py
@@ -0,0 +1,5 @@
+eleks = ["Marta Swiss", "Joseph Bleach", "Olaf Breet", "John Dow", "Jane Dow"]
+toshiba = ["Eric Clark", "Josh Wish", "Sara Conor"]
+
+toshiba.append(eleks)
+print(toshiba)
---
+++ Homeworks/Lesson3/task3.py
@@ -0,0 +1,3 @@
+omnivorous = []
+
+vegetarian = []
