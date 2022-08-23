6e89dfc1cc485ec949f8c95764559d790eef6140
Андрей Войнено <andriivoinenko@MacBook-Air-Andrej.local>
2022-08-23 21:35:41 +0300

    lesson3_homework

--- Homeworks/Lesson3/task3.py
+++ Homeworks/Lesson3/task3.py
@@ -1,3 +1,7 @@
-omnivorous = []
+# I don't understand this task at all
+omnivorous = ['Clark', 'Villie']
 
-vegetarian = []
+vegetarian = ['Yori', 'Bori', 'Another cringe name']
+
+for name in vegetarian:
+    print(name)
